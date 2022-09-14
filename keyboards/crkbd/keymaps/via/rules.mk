bab309929f52d9517a29fcfe362d686a11cbdd75
andreas loeffler <al@exitzero.de>
2022-09-14 19:58:01 +0200

    r the commit message for your changes. Lines starting

--- keyboards/crkbd/keymaps/via/rules.mk
+++ keyboards/crkbd/keymaps/via/rules.mk
@@ -1,6 +1,8 @@
 MOUSEKEY_ENABLE = no     # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
+COMBO_ENABLE    = yes
+
 LTO_ENABLE      = yes
